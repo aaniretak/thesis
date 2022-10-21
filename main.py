900e38610eda60f0bb2a5e8d85f8b75e27e37392
aaniretak <82287377+aaniretak@users.noreply.github.com>
2022-10-21 17:39:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ NASNet = ["https://tfhub.dev/google/imagenet/nasnet_large/classification/5",[1,3
 EfficientNet = ["https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_m/classification/2",[1,480,480,3],'../Datasets/ImageNet',0]
 
 model_dict = {'1':MobileNet,'2':ResNet,'3':Inception,'4':NASNet,'5':EfficientNet}
-filename = 'batches'+task+'.txt
+
 
 for batch in [2**i for i in range(6)]:
     
@@ -27,7 +27,7 @@ for batch in [2**i for i in range(6)]:
 
         inf_dict = modelInference(model_dict[task],21,321)
         inf_dict['batch'] = batch
-        f = open(filename, "a")
+        f = open('batches5.txt', "a")
         f.write(str(inf_dict)+'\n')
         f.close()
         print("For batchsize = {}:\n\tThoughput = {}samples/sec\n\tLatency(All) = {}sec\n------".format(batch,inf_dict['throughput'],sum(inf_dict['latency'])))
